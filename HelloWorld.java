23a6443831580e97edb862055d97ce85148c23c0
Svyatoslav_Kovilskiy <s.kovylskiy@yandex.ru>
2022-07-16 09:57:42 +0500

    Chenged phrase

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world! It's me Svyatoslav");
+		System.out.println("Hello world! I love Kebab");
 	}
 }
